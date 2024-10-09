5b4407aef4b7b205ec9f8c99af1d5f46ae09cbd7
B3Codes <42819145+B3Codes@users.noreply.github.com>
2024-10-10 00:21:55 +0530

    Time: 54 ms (63.04%) | Memory: 12.9 MB (65.53%) - LeetSync

--- 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
+++ 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     int partitionString(string s) {
-        int j = 1, count = 0;
+        int count = 0;
         int n = s.length();
         string temp = "";
-        // temp = temp + s[0];
         for(int i = 0;i<n;i++) {
             auto found = temp.find(s[i]);
 
             if(found != string::npos){
                 count ++;
                 temp = "";
-                
-                
             } 
-            temp = temp + s[i];
             
+            temp = temp + s[i];
         }
         
 
