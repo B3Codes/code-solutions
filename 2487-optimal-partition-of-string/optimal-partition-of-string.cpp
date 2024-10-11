a190389b1910d2482a6cc1dfa079e9516c5b7f26
B3Codes <42819145+B3Codes@users.noreply.github.com>
2024-10-11 15:51:35 +0530

    Time: 165 ms (22.08%) | Memory: 47.2 MB (37.07%) - LeetSync

--- 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
+++ 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
@@ -1,20 +1,39 @@
 class Solution {
 public:
     int partitionString(string s) {
-        int count = 0;
-        int n = s.length();
-        string temp = "";
-        for(int i = 0;i<n;i++) {
-            auto found = temp.find(s[i]);
+        // int count = 0;
+        // int n = s.length();
+        // string temp = "";
+        // for(int i = 0;i<n;i++) {
+        //     auto found = temp.find(s[i]);
 
-            if(found != string::npos){
-                count ++;
-                temp = "";
-            } 
+        //     if(found != string::npos){
+        //         count ++;
+        //         temp = "";
+        //     } 
             
-            temp = temp + s[i];
-        }
+        //     temp = temp + s[i];
+        // }
         
+
+        // return count + 1;
+
+
+// Optimization: using set - TC: O(n)
+
+        int count = 0;
+        int n = s.size();
+
+        unordered_set<char> seenSet;
+
+        for(int i = 0;i<n;i++) {
+            if(seenSet.find(s[i]) != seenSet.end()){
+                count++;
+                seenSet.clear();
+            }
+
+            seenSet.insert(s[i]);
+        }
 
         return count + 1;
     }
