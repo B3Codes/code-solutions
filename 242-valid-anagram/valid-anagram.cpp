04dfa6a49641e62dca91e4e1287ec852e24b1426
B3Codes <42819145+B3Codes@users.noreply.github.com>
2024-10-18 18:55:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (54.89%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -3,22 +3,41 @@ public:
     bool isAnagram(string s, string t) {
         if(s.size() != t.size())
             return false;
-        map<char, int> strMap;
+        // map<char, int> strMap;
 
-        for(int i = 0;i< s.size(); i++) {
-            strMap[s[i]]++;
+        // for(int i = 0;i< s.size(); i++) {
+        //     strMap[s[i]]++;
+        // }
+
+        // for(int i = 0;i<t.size();i++){
+        //     if(strMap.find(t[i]) == strMap.end()){
+        //         return false;
+        //     } else {
+        //         strMap[t[i]]--;
+        //     }
+        // }
+
+        // for(auto x: strMap){
+        //     if(x.second != 0)
+        //         return false;
+        // }
+
+        // return true;
+
+        // Using array
+
+        int charArr[26] = {0};
+
+        for(auto x: s){
+            charArr[x - 'a']++;
         }
 
-        for(int i = 0;i<t.size();i++){
-            if(strMap.find(t[i]) == strMap.end()){
-                return false;
-            } else {
-                strMap[t[i]]--;
-            }
+        for(auto x: t){
+            charArr[x - 'a']--;
         }
 
-        for(auto x: strMap){
-            if(x.second != 0)
+        for(auto x: charArr){
+            if(x != 0)
                 return false;
         }
 
