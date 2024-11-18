0e16433e615968bcd897604cf71144d1fca168bb
B3Codes <42819145+B3Codes@users.noreply.github.com>
2024-11-18 18:59:06 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (86.74%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,69 +1,107 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int n = s.size();  // Get the size of the input string
-        int res = 0;       // Variable to store the resulting integer
-        bool negNum = false; // Flag to indicate if the number is negative
+        // int n = s.size();  // Get the size of the input string
+        // int res = 0;       // Variable to store the resulting integer
+        // bool negNum = false; // Flag to indicate if the number is negative
 
+        // int i = 0;
+
+        // // Skip leading whitespaces
+        // while (s[i] == ' ') {
+        //     i++;
+        // }
+
+        // // Check the first non-space character
+        // int firstChar = s[i];
+
+        // // If the first character is '-', set negNum to true (negative number)
+        // if (firstChar == '-') {
+        //     negNum = true;
+        // } 
+        // // If the first character is '+', just continue (positive number)
+        // else if (firstChar == '+') {
+        //     negNum = false;
+        // } 
+        // // If the first character is not a digit or sign, return 0 (invalid input)
+        // else if (firstChar - '0' < 0 || firstChar - '0' > 9) {
+        //     return 0;
+        // } 
+        // // If the first character is a valid digit, initialize the result with it
+        // else if (firstChar - '0' >= 0 && firstChar - '0' < 10) {
+        //     res = firstChar - '0';
+        // }
+
+        // // Process the remaining characters
+        // for (i = i + 1; i < n; i++) {
+        //     int digit = s[i] - '0'; // Convert current character to an integer digit
+
+        //     // If the character is not a valid digit, stop processing
+        //     if (digit < 0 || digit > 9) {
+        //         break;
+        //     }
+
+        //     // If the number is negative
+        //     if (negNum) {
+        //         // Check for overflow when subtracting the digit
+        //         if (res < (INT_MIN + digit) / 10) {
+        //             res = INT_MIN; // Set to INT_MIN if overflow occurs
+        //             break;
+        //         } else {
+        //             res = res * 10 - digit; // Build the negative number
+        //         }
+        //     } 
+        //     // If the number is positive
+        //     else {
+        //         // Check for overflow when adding the digit
+        //         if (res > (INT_MAX - digit) / 10) {
+        //             res = INT_MAX; // Set to INT_MAX if overflow occurs
+        //             break;
+        //         } else {
+        //             res = res * 10 + digit; // Build the positive number
+        //         }
+        //     }
+        // }
+
+        // // Return the result after processing all valid digits
+        // return res;
+
+        int n = s.size();
         int i = 0;
+        int res = 0;
+        bool negNum = false;
 
-        // Skip leading whitespaces
-        while (s[i] == ' ') {
+        // skip leading whitespaces
+        while(i < n && s[i] == ' '){
             i++;
         }
 
-        // Check the first non-space character
-        int firstChar = s[i];
-
-        // If the first character is '-', set negNum to true (negative number)
-        if (firstChar == '-') {
-            negNum = true;
-        } 
-        // If the first character is '+', just continue (positive number)
-        else if (firstChar == '+') {
-            negNum = false;
-        } 
-        // If the first character is not a digit or sign, return 0 (invalid input)
-        else if (firstChar - '0' < 0 || firstChar - '0' > 9) {
-            return 0;
-        } 
-        // If the first character is a valid digit, initialize the result with it
-        else if (firstChar - '0' >= 0 && firstChar - '0' < 10) {
-            res = firstChar - '0';
+        // Handle sign (if any)
+        if(i<n && s[i] == '-' || s[i] == '+'){
+            negNum = (s[i] == '-');
+            i++;
         }
 
-        // Process the remaining characters
-        for (i = i + 1; i < n; i++) {
-            int digit = s[i] - '0'; // Convert current character to an integer digit
+        // Process the digits
+        while(i <n && isdigit(s[i])){
+            int digit = s[i] - '0';
 
-            // If the character is not a valid digit, stop processing
-            if (digit < 0 || digit > 9) {
-                break;
-            }
+            if(negNum){
+                if(res < (INT_MIN + digit)/10){
+                    return INT_MIN;
+                }
 
-            // If the number is negative
-            if (negNum) {
-                // Check for overflow when subtracting the digit
-                if (res < (INT_MIN + digit) / 10) {
-                    res = INT_MIN; // Set to INT_MIN if overflow occurs
-                    break;
-                } else {
-                    res = res * 10 - digit; // Build the negative number
+                res = res * 10 - digit;
+            } else {
+                if(res > (INT_MAX - digit)/10){
+                    return INT_MAX;
                 }
-            } 
-            // If the number is positive
-            else {
-                // Check for overflow when adding the digit
-                if (res > (INT_MAX - digit) / 10) {
-                    res = INT_MAX; // Set to INT_MAX if overflow occurs
-                    break;
-                } else {
-                    res = res * 10 + digit; // Build the positive number
-                }
+
+                res = res * 10 + digit;
             }
+            i++;
         }
 
-        // Return the result after processing all valid digits
         return res;
     }
 };
