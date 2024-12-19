0080d28b46d50959e98b497388392eeed6617150
Bhanu Pratap <bhnpratap30@gmail.com>
2024-12-19 13:32:27 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (81.58%) - LeetSync

--- 2881-split-strings-by-separator/split-strings-by-separator.cpp
+++ 2881-split-strings-by-separator/split-strings-by-separator.cpp
@@ -2,21 +2,39 @@ class Solution {
 public:
     vector<string> splitWordsBySeparator(vector<string>& words, char separator) {
         vector<string> res;
-        for(int i = 0;i<words.size();i++){
-            string temp = words[i];
-            string charTemp = "";
-            for(int c = 0; c<temp.size();c++){
-                if(temp[c] == separator){
-                    if(charTemp != "")
-                    res.push_back(charTemp);
-                    charTemp = "";
-                    continue;
-                }
+        // for(int i = 0;i<words.size();i++){
+        //     string temp = words[i];
+        //     string charTemp = "";
+        //     for(int c = 0; c<temp.size();c++){
+        //         if(temp[c] == separator){
+        //             if(charTemp != "")
+        //             res.push_back(charTemp);
+        //             charTemp = "";
+        //             continue;
+        //         }
 
-                charTemp.push_back(temp[c]);
+        //         charTemp.push_back(temp[c]);
+        //     }
+        //     if(charTemp != "")
+        //     res.push_back(charTemp);
+        // }
+
+        for(const auto& word : words) {
+            string currentSegment;
+            for(char ch: word) {
+                if(ch == separator) {
+                    if(!currentSegment.empty()){
+                    res.push_back(currentSegment);
+                    currentSegment.clear();
+                    }
+                } else {
+                    currentSegment.push_back(ch);
+                }
             }
-            if(charTemp != "")
-            res.push_back(charTemp);
+
+            if(!currentSegment.empty()){
+                res.push_back(currentSegment);
+            }
         }
 
         return res;
