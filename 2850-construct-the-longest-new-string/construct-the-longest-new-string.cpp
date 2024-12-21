3264db3e69d562bbbe5fb46be6567c442eb35485
Bhanu Pratap <bhnpratap30@gmail.com>
2024-12-21 17:35:43 +0530

    Time: 2 ms (35.54%) | Memory: 9.6 MB (73.49%) - LeetSync

--- 2850-construct-the-longest-new-string/construct-the-longest-new-string.cpp
+++ 2850-construct-the-longest-new-string/construct-the-longest-new-string.cpp
@@ -1,34 +1,8 @@
 class Solution {
 public:
     int longestString(int x, int y, int z) {
-        string res = "";
-        while(z){
-            res = res + "AB";
-            z--;
-        }
+        return 2*z + 4*min(x,y) + ((x != y) ? 2 : 0);
 
-        bool xTurn = true;
-        (x > y) ? xTurn = true : xTurn = false;
-        while(x && y){
-            if(xTurn){
-                res = res + "AA";
-                x--;
-            } else {
-                res = res + "BB";
-                y--;
-            }
-            xTurn = !xTurn;
-        }
-
-        if(x){
-            res = res + "AA";
-        }
-
-        if(y){
-            res = res + "BB";
-        }
-
-        return res.size();
 
     }
 };
