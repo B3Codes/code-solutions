882a62005531865939160d4d9d95c647d885f96a
Bhanu Pratap <bhnpratap30@gmail.com>
2025-01-16 18:13:23 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (53.53%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -13,19 +13,38 @@ public:
     // }
 
 // Optimizing with Memoization : top-down approach
-int robHelper(vector<int>& nums, vector<int> &dp, int n) {
-        if(n < 0)
-            return 0;
+// int robHelper(vector<int>& nums, vector<int> &dp, int n) {
+//         if(n < 0)
+//             return 0;
 
-            if(dp[n] != -1) {
-                return dp[n];
-            }
+//             if(dp[n] != -1) {
+//                 return dp[n];
+//             }
         
-        return dp[n] = max(robHelper(nums, dp, n-1), robHelper(nums, dp, n-2) + nums[n]);
-    }
+//         return dp[n] = max(robHelper(nums, dp, n-1), robHelper(nums, dp, n-2) + nums[n]);
+//     }
 
+//     int rob(vector<int>& nums) {
+//         vector<int> dp(nums.size() + 1, -1);
+//         return robHelper(nums, dp, nums.size() - 1);
+//     }
+
+
+// Bottom-Up Dynamic Programming
     int rob(vector<int>& nums) {
+        int n= nums.size();
+        if(n == 0 )
+            return 0;
+        if(n == 1)
+            return nums[0];
+
         vector<int> dp(nums.size() + 1, -1);
-        return robHelper(nums, dp, nums.size() - 1);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        for(int i = 2;i<n; i++) {
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i]);
+        }
+        return dp[n-1];
     }
 };
