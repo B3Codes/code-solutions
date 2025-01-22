29608003a7de017bfbe0d73c47e7abd36f0bb8e0
Bhanu Pratap <bhnpratap30@gmail.com>
2025-01-22 17:49:02 +0530

    Time: 31 ms (55.43%) | Memory: 26.7 MB (41.32%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -32,17 +32,51 @@ public:
    
 
 
+    // int coinChange(vector<int>& coins, int amount) {
+    //     vector<int> dp(amount + 1, INT_MAX);
+    //     dp[0] = 0;
+
+    //     for(int i = 1;i<amount+1;i++){
+    //         for(int coin: coins){
+    //             if(coin <= i && dp[i - coin] != INT_MAX)
+    //                 dp[i] = min(dp[i], dp[i - coin] + 1); 
+    //         }
+    //     }
+
+    //     return (dp[amount] != INT_MAX ? dp[amount] : -1);
+    // }
+
+
+    /* using BFS approach */
+
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount + 1, INT_MAX);
-        dp[0] = 0;
+        if (amount == 0)
+            return 0;
+        queue<pair<int, int>> q;
+        q.push({0,0});  // start from amount = 0, coins = 0
 
-        for(int i = 1;i<amount+1;i++){
-            for(int coin: coins){
-                if(coin <= i && dp[i - coin] != INT_MAX)
-                    dp[i] = min(dp[i], dp[i - coin] + 1); 
+        vector<int> visited(amount + 1, false);
+        visited[0] = true;
+
+        while(!q.empty()) {
+            auto [currentAmount, steps] = q.front();
+            q.pop();
+
+            for(int coin: coins) {
+                int nextAmount = currentAmount + coin;
+
+                // if target amount is reached, return the steps
+                if(nextAmount == amount) return steps + 1;
+
+                // if amount is valid and still is not reached
+                if(nextAmount < amount + 1 && !visited[nextAmount]) {
+                    visited[nextAmount] = true;
+                    q.push({nextAmount, steps + 1});
+                }
+
             }
-        }
+         }
 
-        return (dp[amount] != INT_MAX ? dp[amount] : -1);
+         return -1;
     }
 };
