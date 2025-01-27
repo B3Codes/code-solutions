60d5300ecc1bc98f182b4564042f6c4782f83dd3
Bhanu Pratap <bhnpratap30@gmail.com>
2025-01-27 21:45:56 +0530

    Time: 79 ms (15.94%) | Memory: 14.1 MB (76.54%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,56 +1,22 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int sum;
-        vector<int> sumInd;
-        int n = nums.size();
-        // sort(nums.begin(), nums.end());
-        map<int,int> sumMap;
-        int i = 0, j=n-1;
-    //     for(int i =0;i<n;i++){
-    //         for(int j = i+1;j<n;j++){
-    //         sum = nums[i] + nums[j];
-    //         if(sum == target) {
-    //             sumInd = {i,j};
-    //             break;
-    //         }
-    //     }
-    // }
+        vector<int> res;
+        for(int i = 0;i<nums.size()-1;i++) {
+            int x = nums[i];
+            for(int j = i+1;j<nums.size();j++) {
+                int rem;
+                
+                    rem = target -  x;
+                if((nums[j]) == rem) {
+                    res.push_back(i);
+                    res.push_back(j);
+                    return res;
+                }
 
-    // while(i<n) {
-
-    // }
-
-    for(int i =0;i<n;i++){
-        int x = target - nums[i];
-        // if(sumMap.empty() && sumMap.find(x) == sumMap.end()){
-        //     sumMap[x] = i;
-        // } else {
-        //     sumInd = {sumMap[x], i};
-        //     break;
-        // }
-
-        if(sumMap.find(x) != sumMap.end()) {
-            return {sumMap[x],i};
+            }
         }
-        sumMap[nums[i]] = i;
-    }
 
-
-
-    
-//     while (i <j){
-//     int sum = nums[i] + nums[j];
-//     if(sum < target) {
-//         i++;
-//     } else if(sum > target)
-//         j--;
-//     else
-// {        sumInd = {i,j};
-//         break;
-//         }
-
-// }
-    return {-1,-1};
+        return res;
     }
 };
