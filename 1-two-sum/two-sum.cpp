7aaf1681571e86fa44642579b0172669c92b7a31
Bhanu Pratap <bhnpratap30@gmail.com>
2025-01-27 21:46:21 +0530

    Time: 46 ms (23.59%) | Memory: 13.9 MB (92.37%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -5,10 +5,7 @@ public:
         for(int i = 0;i<nums.size()-1;i++) {
             int x = nums[i];
             for(int j = i+1;j<nums.size();j++) {
-                int rem;
-                
-                    rem = target -  x;
-                if((nums[j]) == rem) {
+               if((nums[j]) == target -  x) {
                     res.push_back(i);
                     res.push_back(j);
                     return res;
