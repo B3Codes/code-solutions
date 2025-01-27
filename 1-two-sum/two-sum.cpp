89a94e2dbcdad13264245e99e2e718b785213c55
Bhanu Pratap <bhnpratap30@gmail.com>
2025-01-27 22:04:45 +0530

    Time: 3 ms (74.73%) | Memory: 15.1 MB (15.23%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -2,15 +2,30 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> res;
-        for(int i = 0;i<nums.size()-1;i++) {
-            int x = nums[i];
-            for(int j = i+1;j<nums.size();j++) {
-               if((nums[j]) == target -  x) {
-                    res.push_back(i);
-                    res.push_back(j);
-                    return res;
-                }
+        // for(int i = 0;i<nums.size()-1;i++) {
+        //     int x = nums[i];
+        //     for(int j = i+1;j<nums.size();j++) {
+        //        if((nums[j]) == target -  x) {
+        //             res.push_back(i);
+        //             res.push_back(j);
+        //             return res;
+        //         }
 
+        //     }
+        // }
+
+        // using hashmap
+
+        map<int, int> numsMap;
+
+        for(int i = 0;i<nums.size();i++){
+            int compliment = target - nums[i];
+            if(numsMap.find(compliment) != numsMap.end()) {
+                res.push_back(numsMap[compliment]);
+                res.push_back(i);
+                return res;
+            } else {
+                numsMap[nums[i]] = i;
             }
         }
 
