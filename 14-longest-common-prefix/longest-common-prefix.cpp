844a6deb4cbb8e97e802a19a76e9e244dd94f906
Bhanu Pratap <bhnpratap30@gmail.com>
2025-02-03 17:57:28 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (9.57%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,5 +1,29 @@
 class Solution {
 public:
+    string commonPrefix(const string &leftPrefix,  const string &rightPrefix) {
+        int i = 0;
+        while(i < leftPrefix.size() && i < rightPrefix.size() && leftPrefix[i] == rightPrefix[i]) {
+            i++;
+        }
+
+        return leftPrefix.substr(0,i);
+    }
+    string longestCommonPrefixHelper(vector<string>& strs, int start, int end) {
+        if(start == end)
+            return strs[start];
+
+        int mid = (end + start)/2;
+
+        string leftPrefix = longestCommonPrefixHelper(strs, start, mid);
+        string rightPrefix = longestCommonPrefixHelper(strs, mid+1, end);
+        
+        return commonPrefix(leftPrefix, rightPrefix);
+
+
+
+    }
+
+
     string longestCommonPrefix(vector<string>& strs) {
         if(strs.empty())
             return "";
@@ -32,22 +56,25 @@ public:
     // return strs[0];
 
 // sorting & compare first & last
-    sort(strs.begin(), strs.end());
+    // sort(strs.begin(), strs.end());
 
-    // if(strs.size() == 1 || strs[0].size() == 1)
+    // // if(strs.size() == 1 || strs[0].size() == 1)
+    // //     return strs[0];
+    // int i = 0;
+    // for(;i<strs[0].size();i++) {
+    //     if(strs[0][i] != strs[strs.size() - 1][i]) {
+    //         return strs[0].substr(0, i);
+    //     }
+    // }
+
+    // if(i == strs[0].size())
     //     return strs[0];
-    int i = 0;
-    for(;i<strs[0].size();i++) {
-        if(strs[0][i] != strs[strs.size() - 1][i]) {
-            return strs[0].substr(0, i);
-        }
-    }
+    // else
+    //     return "";
 
-    if(i == strs[0].size())
-        return strs[0];
-    else
-        return "";
 
+// Divide and Conquer
+    return longestCommonPrefixHelper(strs, 0, strs.size() - 1);
 
     }
 };
