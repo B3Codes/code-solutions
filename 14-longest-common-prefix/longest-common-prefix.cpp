1e4baae0f2bd4f3c7221e262338d43155aac98a1
Bhanu Pratap <bhnpratap30@gmail.com>
2025-02-03 18:00:38 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (9.53%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,32 +1,9 @@
 class Solution {
 public:
-    string commonPrefix(const string &leftPrefix,  const string &rightPrefix) {
-        int i = 0;
-        while(i < leftPrefix.size() && i < rightPrefix.size() && leftPrefix[i] == rightPrefix[i]) {
-            i++;
-        }
 
-        return leftPrefix.substr(0,i);
-    }
-    string longestCommonPrefixHelper(vector<string>& strs, int start, int end) {
-        if(start == end)
-            return strs[start];
-
-        int mid = (end + start)/2;
-
-        string leftPrefix = longestCommonPrefixHelper(strs, start, mid);
-        string rightPrefix = longestCommonPrefixHelper(strs, mid+1, end);
-        
-        return commonPrefix(leftPrefix, rightPrefix);
-
-
-
-    }
-
-
-    string longestCommonPrefix(vector<string>& strs) {
-        if(strs.empty())
-            return "";
+// string longestCommonPrefix(vector<string>& strs) {
+//     if(strs.empty())
+//         return "";
 
 // naive approach (Horizontal scanning)
         // string intialPrefix = strs[0];
@@ -71,8 +48,35 @@ public:
     //     return strs[0];
     // else
     //     return "";
+    // }
+
+    string commonPrefix(const string &leftPrefix,  const string &rightPrefix) {
+        int i = 0;
+        while(i < leftPrefix.size() && i < rightPrefix.size() && leftPrefix[i] == rightPrefix[i]) {
+            i++;
+        }
+
+        return leftPrefix.substr(0,i);
+    }
+    string longestCommonPrefixHelper(vector<string>& strs, int start, int end) {
+        if(start == end)
+            return strs[start];
+
+        int mid = (end + start)/2;
+
+        string leftPrefix = longestCommonPrefixHelper(strs, start, mid);
+        string rightPrefix = longestCommonPrefixHelper(strs, mid+1, end);
+        
+        return commonPrefix(leftPrefix, rightPrefix);
 
 
+
+    }
+
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty())
+            return "";
+    
 // Divide and Conquer
     return longestCommonPrefixHelper(strs, 0, strs.size() - 1);
 
