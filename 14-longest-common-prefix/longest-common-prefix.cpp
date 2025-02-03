1d550319c9cf074fc8826d5d5ecb008c59abb17a
Bhanu Pratap <bhnpratap30@gmail.com>
2025-02-03 17:34:20 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (70.31%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -20,15 +20,34 @@ public:
 // vertical scanning
     // string intial = strs[0];
 
-    for(int i  = 0;i<strs[0].size();i++) {
-        char ch = strs[0][i];
-        for(int j = 1;j<strs.size();j++) {
-            if(i >= strs[j].size() || strs[j][i] != ch) {
-                return strs[0].substr(0, i);
-            }
+    // for(int i  = 0;i<strs[0].size();i++) {
+    //     char ch = strs[0][i];
+    //     for(int j = 1;j<strs.size();j++) {
+    //         if(i >= strs[j].size() || strs[j][i] != ch) {
+    //             return strs[0].substr(0, i);
+    //         }
+    //     }
+    // }
+
+    // return strs[0];
+
+// sorting & compare first & last
+    sort(strs.begin(), strs.end());
+
+    // if(strs.size() == 1 || strs[0].size() == 1)
+    //     return strs[0];
+    int i = 0;
+    for(;i<strs[0].size();i++) {
+        if(strs[0][i] != strs[strs.size() - 1][i]) {
+            return strs[0].substr(0, i);
         }
     }
 
-    return strs[0];
+    if(i == strs[0].size())
+        return strs[0];
+    else
+        return "";
+
+
     }
 };
