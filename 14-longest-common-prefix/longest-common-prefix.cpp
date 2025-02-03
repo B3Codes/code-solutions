43b75aee2e576e5e3ccb87b375ff47d496a64927
Bhanu Pratap <bhnpratap30@gmail.com>
2025-02-03 16:55:55 +0530

    Time: 4 ms (10.68%) | Memory: 11.6 MB (99.73%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -4,16 +4,31 @@ public:
         if(strs.empty())
             return "";
 
-        string intialPrefix = strs[0];
+// naive approach (Horizontal scanning)
+        // string intialPrefix = strs[0];
         
-        for(int i = 1;i<strs.size();i++) {
-            while (strs[i].find(intialPrefix) != 0) {
-                intialPrefix = intialPrefix.substr(0, intialPrefix.size() - 1);
-                if(intialPrefix.empty())
-                    return "";
+        // for(int i = 1;i<strs.size();i++) {
+        //     while (strs[i].find(intialPrefix) != 0) {
+        //         intialPrefix = intialPrefix.substr(0, intialPrefix.size() - 1);
+        //         if(intialPrefix.empty())
+        //             return "";
+        //     }
+        // }
+
+        // return intialPrefix;
+
+// vertical scanning
+    // string intial = strs[0];
+
+    for(int i  = 0;i<strs[0].size();i++) {
+        char ch = strs[0][i];
+        for(int j = 1;j<strs.size();j++) {
+            if(i >= strs[j].size() || strs[j][i] != ch) {
+                return strs[0].substr(0, i);
             }
         }
+    }
 
-        return intialPrefix;
+    return strs[0];
     }
 };
