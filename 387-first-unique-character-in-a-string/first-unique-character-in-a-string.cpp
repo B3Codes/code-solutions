cb32e559faf287df02a20da4eddf1b1265c11471
Bhanu Pratap <bhnpratap30@gmail.com>
2025-02-03 14:13:02 +0530

    Time: 23 ms (18.87%) | Memory: 24.3 MB (6.39%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -1,19 +1,34 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        vector<int> freqArr(26, 0);
+        // vector<int> freqArr(26, 0);
 
-        for(int i = 0;i<s.size();i++) {
-            freqArr[s[i] - 'a']++;
+        // for(int i = 0;i<s.size();i++) {
+        //     freqArr[s[i] - 'a']++;
+        // }
+
+        // for(int i =0;i<s.size();i++) {
+        //     if(freqArr[s[i] - 'a'] == 1)
+        //         return i;
+        // }
+
+        // return -1;
+
+
+        int n = s.size();
+
+        unordered_map<char,int> freqMap(n);
+
+        for(int i=0;i<n;i++) {
+            freqMap[s[i]]++;
         }
 
-        for(int i =0;i<s.size();i++) {
-            if(freqArr[s[i] - 'a'] == 1)
+        for(int i=0;i<n;i++) {
+            if(freqMap[s[i]] == 1) {
                 return i;
+            }
         }
 
-        return -1;
-
-         
+         return -1;
     }
 };
