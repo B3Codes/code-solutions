d05b23440307911301bed2af4351c72eee7b7490
Bhanu Pratap <bhnpratap30@gmail.com>
2025-03-04 16:11:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (6.53%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -1,29 +1,46 @@
 class Solution {
 public:
-    // int tribonacciHelper(int n) {
-    //     if(n==0)
-    //         return 0;
-    //     if(n == 1 || n == 2)
-    //         return 1;
-        
-    //     return tribonacciHelper[n] + tribonacciHelper[n-1] + tribonacciHelper[n-2];
-    // }
-    int tribonacci(int n) {
-        if(n == 0)
+    int tribonacciHelper(int n,vector<int>& memo) {
+        if(memo[n] != -1)
+            return memo[n];
+        if(n==0)
             return 0;
-            if(n == 1 || n == 2)
-                return 1;
+        if(n == 1 || n == 2)
+            return 1;
+        
+        memo[n] = tribonacciHelper(n-1, memo) + tribonacciHelper(n-2, memo) + tribonacciHelper(n-3, memo);
+
+        return memo[n];
+    }
+    int tribonacci(int n) {
+        // if(n == 0)
+        //     return 0;
+        //     if(n == 1 || n == 2)
+        //         return 1;
             
 
-        vector<int> tri(n+ 1);
-        tri[0] = 0;
-        tri[1] = 1;
-        tri[2] = 1;
+        // vector<int> tri(n+ 1);
+        // tri[0] = 0;
+        // tri[1] = 1;
+        // tri[2] = 1;
 
-        for(int i = 3;i<=n;i++) {
-            tri[i] = tri[i-1] + tri[i-2] + tri[i-3];
-        }
+        // for(int i = 3;i<=n;i++) {
+        //     tri[i] = tri[i-1] + tri[i-2] + tri[i-3];
+        // }
  
-        return tri[n];
+        // return tri[n];
+
+        // // recursion
+        // if(n == 0)
+        //     return 0;
+
+        // if(n == 1 || n == 2)
+        //     return 1;
+        
+        // return tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3);
+
+        //Recursion with Memoization
+        vector<int> memo(n+1, -1);
+        return tribonacciHelper(n, memo);
     }
 };
